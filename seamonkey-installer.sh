6b97204bfd3f6b9541568a74537fe43a1d182c33
Cian <cian@rhuidean>
2022-05-11 10:46:38 +0200

    Added check for root privelages

--- seamonkey-installer.sh
+++ seamonkey-installer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+if [[ $EUID != 0 ]]; then
+		echo "Please run as root/sudo"
+		exit
+fi
+
 pkgurl=`curl -s "https://www.seamonkey-project.org/releases/" | grep 'en-GB.linux-x86_64' | cut -d '"' -f 2`
 pkgname=`echo $pkgurl | cut -d '/' -f 10 | cut -d '"' -f 1`
 
-# echo $pkgurl
-# echo $pkgname
 wget $pkgurl
 tar -xf $pkgname -C /opt
 cp ./seamonkey.desktop /usr/share/applications/seamonkey.desktop
--- seamonkey-uninstaller.sh
+++ seamonkey-uninstaller.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID != 0 ]]; then
+		echo "Please run as root"
+		exit
+fi
+
 rm -r /opt/seamonkey
 rm /usr/share/applications/seamonkey.desktop
 rm /usr/bin/seamonkey
